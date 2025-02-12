b2efc59ec22774126ed0d130ef11805c037fa472
julien382 <julhermain@gmail.com>
2025-02-12 15:53:06 +0100

    create mainAbove

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -39,107 +39,109 @@ const ContactPage = () => {
 
   return (
     <div className="contactPage">
-      <h1>Contactez-</h1>
-            <h1>Nous</h1>
-            <div className='contactSocialMedia'>
+        <h1>Contactez-</h1>
+        <h1>Nous</h1>
 
+        <div className='contactSocialMedia'>
+
+        </div>
+
+        <div className='contactInfo'>
+            <div className='containerLocalisation'>
+                <p className='FooterNameType'>Localisation</p>
+                <Link to="https://www.linkedin.com/in/julien-hermain/">
+                    <p>Hauts-De-France</p>
+                </Link>
+                <Link to="https://www.linkedin.com/in/julien-hermain/">
+                    <p>Merlimont</p>
+                </Link>
+                <Link to="https://www.linkedin.com/in/julien-hermain/">
+                    <p>Berck</p>
+                </Link>
+                <Link to="https://www.linkedin.com/in/julien-hermain/">
+                    <p>Le Touquet</p>
+                </Link>
             </div>
-            <div className='contactInfo'>
-                <div className='containerLocalisation'>
-                    <p className='FooterNameType'>Localisation</p>
-                    <Link to="https://www.linkedin.com/in/julien-hermain/">
-                        <p>Hauts-De-France</p>
-                    </Link>
-                    <Link to="https://www.linkedin.com/in/julien-hermain/">
-                        <p>Merlimont</p>
-                    </Link>
-                    <Link to="https://www.linkedin.com/in/julien-hermain/">
-                        <p>Berck</p>
-                    </Link>
-                    <Link to="https://www.linkedin.com/in/julien-hermain/">
-                        <p>Le Touquet</p>
-                    </Link>
+            <div className='containerContact'>
+                <p className='FooterNameType'>Contact</p>
+                <Link to="https://www.linkedin.com/in/julien-hermain/">
+                    <p>Merliweb@*****.**</p>
+                </Link>
+                <Link to="https://www.linkedin.com/in/julien-hermain/">
+                    <p>06 ** ** ** **</p>
+                </Link>
+            </div>
+        </div>
+
+        <img src={contact} className="imageContact" alt="Merliweb Logo" />
+
+        <div className="contactForm">
+            {submitted ? (
+            <p className="successMessage">Merci pour votre message !</p>
+            ) : (
+            <form onSubmit={handleSubmit} className="formContainer">
+                <div className="formGroup">
+                <label>Prénom</label>
+                <input
+                    type="text"
+                    name="firstName"
+                    value={formData.firstName}
+                    onChange={handleChange}
+                    placeholder="Prénom"
+                />
+                {errors.firstName && <p className="error">{errors.firstName}</p>}
                 </div>
-                <div className='containerContact'>
-                    <p className='FooterNameType'>Contact</p>
-                    <Link to="https://www.linkedin.com/in/julien-hermain/">
-                        <p>Merliweb@*****.**</p>
-                    </Link>
-                    <Link to="https://www.linkedin.com/in/julien-hermain/">
-                        <p>06 ** ** ** **</p>
-                    </Link>
+
+                <div className="formGroup">
+                <label>Nom</label>
+                <input
+                    type="text"
+                    name="lastName"
+                    value={formData.lastName}
+                    onChange={handleChange}
+                    placeholder="Nom"
+                />
+                {errors.lastName && <p className="error">{errors.lastName}</p>}
                 </div>
-            </div>
 
-            <img src={contact} className="imageContact" alt="Merliweb Logo" />
+                <div className="formGroup">
+                <label>Email *</label>
+                <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="Email"
+                />
+                {errors.email && <p className="error">{errors.email}</p>}
+                </div>
 
-      <div className="contactForm">
-        {submitted ? (
-          <p className="successMessage">Merci pour votre message !</p>
-        ) : (
-          <form onSubmit={handleSubmit} className="formContainer">
-            <div className="formGroup">
-              <label>Prénom</label>
-              <input
-                type="text"
-                name="firstName"
-                value={formData.firstName}
-                onChange={handleChange}
-                placeholder="Prénom"
-              />
-              {errors.firstName && <p className="error">{errors.firstName}</p>}
-            </div>
+                <div className="formGroup">
+                <label>Téléphone</label>
+                <input
+                    type="text"
+                    name="phone"
+                    value={formData.phone}
+                    onChange={handleChange}
+                    placeholder="Téléphone"
+                />
+                </div>
 
-            <div className="formGroup">
-              <label>Nom</label>
-              <input
-                type="text"
-                name="lastName"
-                value={formData.lastName}
-                onChange={handleChange}
-                placeholder="Nom"
-              />
-              {errors.lastName && <p className="error">{errors.lastName}</p>}
-            </div>
+                <div className="formGroup">
+                <label>Message *</label>
+                <textarea
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    placeholder="Message"
+                />
+                {errors.message && <p className="error">{errors.message}</p>}
+                </div>
 
-            <div className="formGroup">
-              <label>Email *</label>
-              <input
-                type="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="Email"
-              />
-              {errors.email && <p className="error">{errors.email}</p>}
-            </div>
-
-            <div className="formGroup">
-              <label>Téléphone</label>
-              <input
-                type="text"
-                name="phone"
-                value={formData.phone}
-                onChange={handleChange}
-                placeholder="Téléphone"
-              />
-            </div>
-
-            <div className="formGroup">
-              <label>Message *</label>
-              <textarea
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                placeholder="Message"
-              />
-              {errors.message && <p className="error">{errors.message}</p>}
-            </div>
-
-            <button type="submit" className="submitButton">ENVOYER</button>
-          </form>
-        )}
-      </div>
+                <button type="submit" className="submitButton">ENVOYER</button>
+            </form>
+            )}
+        </div>
 
     </div>
   );
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,9 +1,18 @@
 import './MainPage.scss'
+import mainAboveImage from "../../assets/undraw/contact.svg";
 
 const MainPage = () => {
 
     return (
-        <div>
+        <div className="mainPage">
+
+            <div className="mainAbove">
+                <h1>Surfez sur la vague du digital avec Merliweb</h1>
+                <p>L'agence qui donne vie à votre présence en ligne.</p>
+                <button className="submitButton">Contactez-Nous</button>
+                <img src={mainAboveImage} className="mainAboveImage" alt="mainAboveImage" />
+            </div>
+
         </div>
     )
 }
