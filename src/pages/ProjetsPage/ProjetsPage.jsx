72a80354bbb40db83ba443849a7bae9e594d1aba
julien382 <julhermain@gmail.com>
2025-02-13 23:11:13 +0100

    create content projet

--- src/pages/ProjetsPage/ProjetsPage.jsx
+++ src/pages/ProjetsPage/ProjetsPage.jsx
@@ -1,7 +1,8 @@
 import './ProjetsPage.scss'
 import Above from '../../components/Above/Above'
-import asset from "../../assets/undraw/asset.svg";
 import ActionContact from '../../components/ActionContact/ActionContact';
+import asset from "../../assets/undraw/asset.svg";
+import arrowRight from "../../assets/arrowRight.svg";
 import { Link } from "react-router-dom";
 
 const ProjetsPage = () => {
@@ -16,7 +17,16 @@ const ProjetsPage = () => {
             <div className='projets'>
                 <p>Nos Projets</p>
                 <h2>Découvrez nos réalisations</h2>
-                <div className='projet'></div>
+                <div className='projet'>
+                    <img src={asset} className='projetImage' alt="image projet" />
+                    <div className="projetInfo">
+                        <div className="projetTitle">
+                            <h3>Site vitrine</h3>
+                            <h3>Merliweb</h3>
+                        </div>
+                        <img src={arrowRight} className="projetArrow" alt="arrow" />
+                    </div>
+                </div>
                 <div className='projet'></div>
                 <div className='projet'></div>
                 <div className='projet'></div>
