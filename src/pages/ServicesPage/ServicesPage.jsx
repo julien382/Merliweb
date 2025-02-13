e8c9bc0d0d5fa86094e582775b9d2b953d9b8347
julien382 <julhermain@gmail.com>
2025-02-13 19:34:29 +0100

    create serviceWeb

--- src/pages/ServicesPage/ServicesPage.jsx
+++ src/pages/ServicesPage/ServicesPage.jsx
@@ -10,6 +10,15 @@ const ServicesPage = () => {
                 title={"Nos Services"} 
                 text={"Du développement web à la communication digitale, nous créons des solutions sur-mesure pour renforcer votre présence en ligne et atteindre vos objectifs. Voici nos services :"}
             />
+            <div className='servicesWeb'>
+                <p>Nos services</p>
+                <h2>Découvrez nos services Web</h2>
+                <div className='serviceWeb'>
+                    <img src={multitasking} className="serviceWebImage" alt="serviceWebImage" />
+                    <h3 className='serviceWebName'>Design UX/UI</h3>
+                    <p className='serviceWebText'>Création d’interfaces modernes et ergonomiques</p>
+                </div>
+            </div>
         </div>
     )
 }
