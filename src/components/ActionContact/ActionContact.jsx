99896f4cb144f3cdbbbefaf5d6055ec251fd905c
julien382 <julhermain@gmail.com>
2025-02-20 21:42:56 +0100

    add media queries actionContact

--- src/components/ActionContact/ActionContact.jsx
+++ src/components/ActionContact/ActionContact.jsx
@@ -6,11 +6,13 @@ const ActionContact = () => {
     return (
         <div className='actionContact'>
             <img src={contact} className="actionContactImage" alt="Merliweb Logo" />
-            <h2>Prêt à faire briller votre présence en ligne ?</h2>
-            <p className='actionContactText'>{'Chez MerliWeb, nous sommes impatients de collaborer avec vous pour donner vie à vos projets numériques.'}</p>
-            <Link to="/contact">
-                <button className="submitButton">Prendre Contact</button>
-            </Link>
+            <span className='contentTextActionContact'>
+                <h2>Prêt à faire briller votre présence en ligne ?</h2>
+                <p className='actionContactText'>{'Chez MerliWeb, nous sommes impatients de collaborer avec vous pour donner vie à vos projets numériques.'}</p>
+                <Link to="/contact">
+                    <button className="submitButton">Prendre Contact</button>
+                </Link>
+            </span>
         </div>
     )
 }
