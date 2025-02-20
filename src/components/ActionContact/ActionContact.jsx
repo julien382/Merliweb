9aa4153d5304f8b1091f93a23c2eeaeaa8e4db30
julien382 <julhermain@gmail.com>
2025-02-20 16:03:57 +0100

    fix bug redirection page contact

--- src/components/ActionContact/ActionContact.jsx
+++ src/components/ActionContact/ActionContact.jsx
@@ -1,5 +1,6 @@
 import './ActionContact.scss'
 import contact from "../../assets/undraw/contact.svg";
+import { Link } from "react-router-dom";
 
 const ActionContact = () => {
     return (
@@ -7,7 +8,9 @@ const ActionContact = () => {
             <img src={contact} className="actionContactImage" alt="Merliweb Logo" />
             <h2>Prêt à faire briller votre présence en ligne ?</h2>
             <p className='actionContactText'>{'Chez MerliWeb, nous sommes impatients de collaborer avec vous pour donner vie à vos projets numériques.'}</p>
-            <button className="submitButton">Prendre Contact</button>
+            <Link to="/contact">
+                <button className="submitButton">Prendre Contact</button>
+            </Link>
         </div>
     )
 }
