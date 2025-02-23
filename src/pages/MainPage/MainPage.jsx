1788e459e2e0d4c6f43172d82d3651941ce0a7ef
julien382 <julhermain@gmail.com>
2025-02-23 16:07:04 +0100

    add media queries mainService

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -131,33 +131,36 @@ const MainPage = () => {
                 <img src={building} className="mainAboveImage" alt="mainAboveImage" />
             </div>
 
+            <div className='mainDescription'>
+                <h2>Vous cherchez à développer votre présence en ligne et à attirer plus de clients ?</h2>
+                <p>Merliweb vous accompagne dans la création de votre site web et la gestion de votre communication digitale.
+                Nous vous aidons à optimiser votre visibilité, à capter l’attention de votre audience et à générer des conversions grâce à des solutions modernes et performantes.</p>
+                <img src={arrowBottom} className="arrow" alt="arrow" />    
+            </div>
+
             <div className='mainService'>
-                <div className='containerTextMainService'>
-                    <h2>Vous cherchez à développer votre présence en ligne et à attirer plus de clients ?</h2>
-                    <p>Merliweb vous accompagne dans la création de votre site web et la gestion de votre communication digitale.
-                    Nous vous aidons à optimiser votre visibilité, à capter l’attention de votre audience et à générer des conversions grâce à des solutions modernes et performantes.</p>
-                    <span className='arrowLinkMain'>
-                        <img src={arrowBottom} className="arrow" alt="arrow" />    
-                    </span>
-                </div>
-                <div className='containerTextMainService'>
-                    <h2>Création de sites web sur-mesure</h2>
-                    <p>{"Nous concevons des sites web modernes, fonctionnels et responsive qui s'adaptent à tous vos besoins. Que vous ayez besoin d'un site vitrine, e-commerce, ou blog."}</p>
-                    <span className='arrowLinkMain'>
-                        <Link to="/services">
-                            <img src={arrowRight} className="arrow" alt="arrow" />
-                        </Link>
-                    </span>
+                <div className='webMainService'>
+                    <div className='textMainService'>
+                        <h2>Création de sites web sur-mesure</h2>
+                        <p>{"Nous concevons des sites web modernes, fonctionnels et responsive qui s'adaptent à tous vos besoins. Que vous ayez besoin d'un site vitrine, e-commerce, ou blog."}</p>
+                        <span className='arrowMainService'>
+                            <Link to="/services">
+                                <img src={arrowRight} className="arrow" alt="arrow" />
+                            </Link>
+                        </span>
+                    </div>
                     <img src={programming} className="mainServiceImage" alt="programming" />
                 </div>
-                <div className='containerTextMainService flouMainCreationcontenu'>
-                    <h2>Création de contenu et gestion des réseaux sociaux</h2>
-                    <p>De la photo à la vidéo, en passant par le drone et la GoPro, nous créons du contenu sur mesure et gérons vos réseaux pour maximiser votre impact digital.</p>
-                    <span className='arrowLinkMain'>
-                        <Link to="/services">
-                            <img src={arrowRight} className="arrow" alt="arrow" />
-                        </Link>
-                    </span>
+                <div className='webMainService flouMainCreationcontenu'>
+                    <div className='textMainService'>
+                        <h2>Création de contenu et gestion des réseaux sociaux</h2>
+                        <p>De la photo à la vidéo, en passant par le drone et la GoPro, nous créons du contenu sur mesure et gérons vos réseaux pour maximiser votre impact digital.</p>
+                        <span className='arrowMainService'>
+                            <Link to="/services">
+                                <img src={arrowRight} className="arrow" alt="arrow" />
+                            </Link>
+                        </span>
+                    </div>
                     <img src={studio} className="mainServiceImage" alt="mainServiceImage" />
                 </div>
             </div>
