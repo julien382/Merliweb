884628bee2ea60c1ee49e3d6bc9c979ae893f0fe
julien382 <julhermain@gmail.com>
2025-03-06 14:13:22 +0100

    add content vite config for 404

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.PNG'],
-})
+  assetsInclude: ['**/*.PNG'], // Garde cette config pour inclure les assets PNG
+
+  // Ajoute cette configuration pour éviter la 404 sur le F5
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    outDir: 'dist', // Assure-toi que le dossier de build est correct
+  },
+});
